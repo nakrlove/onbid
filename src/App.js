f300c1800a2d0cc09ce3c8b99b1762b535f375c9
nakrlove <nakrlove@gmail.com>
2024-01-09 19:02:24 +0900

    메인화면 변경

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Menu from './Menu';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,10 +17,10 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
 
         <Menu />
-      </header>
+      {/* </header> */}
     </div>
   );
 }
